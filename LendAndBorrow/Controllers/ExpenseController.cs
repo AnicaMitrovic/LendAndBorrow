362a947a2b17352364fdbc408914c67bd7184243
AnicaMitrovic <anica.mitrovicc@gmail.com>
2021-02-17 21:19:50 +0100

    - Added Expenses page

---
+++ LendAndBorrow/Controllers/ExpenseController.cs
@@ -0,0 +1,42 @@
+﻿using LendAndBorrow.Data;
+using LendAndBorrow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LendAndBorrow.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ExpenseController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objectList = _dbContext.Expenses;
+            return View(objectList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _dbContext.Expenses.Add(obj);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- LendAndBorrow/Data/AppDbContext.cs
+++ LendAndBorrow/Data/AppDbContext.cs
@@ -14,5 +14,7 @@ namespace LendAndBorrow.Data
         }
 
         public DbSet<Item> Items { get; set; }
+
+        public DbSet<Expense> Expenses { get; set; }
     }
 }
